ec323d2d51a7ae6c400f898a5a6caff4840eb4b1
Victoria <32179813+victoriafomina@users.noreply.github.com>
2022-08-15 14:30:23 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,8 @@ module.exports = {
     utbot_cpp_releases_info: `https://api.github.com/repos/UnitTestBot/UTBotCpp/releases`,
     utbot_java_releases_link: `https://github.com/UnitTestBot/UTBotJava/releases`,
     utbot_java_releases_info: `https://api.github.com/repos/UnitTestBot/UTBotJava/releases`,
-    build_number: `19`,
-    build_date: `2022-6-30`,
+    build_number: `27`,
+    build_date: `2022-8-15`,
   },
 
   plugins: [
