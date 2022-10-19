7b2a5ed0a48ed338c7892dbea00bd416f7a7e0a2
Alena Lisevych <>
2022-10-19 12:41:45 +0300

    Moved settings to system properties in build.gradle.

--- UTBotSiteTest/src/test/java/org/utbot/site/UTBotSiteTest.java
+++ UTBotSiteTest/src/test/java/org/utbot/site/UTBotSiteTest.java
@@ -1,19 +1,9 @@
 package org.utbot.site;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-
 import com.codeborne.selenide.junit5.TextReportExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({TextReportExtension.class})
 public abstract class UTBotSiteTest {
-
-    @BeforeAll
-    public static void setup() {
-        Configuration.baseUrl = UTBotSite.SITE_URL;
-        Configuration.browserSize = "1920x1080";
-        Configuration.headless = true;
-    }
 
 }
