1e0c2573ea2bb7e2eddd0936a234f3e54744cfed
victoriafomina <viky.fomina@yandex.ru>
2022-11-01 19:27:25 +0300

    Update gifs and images on wide screens

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import cn from "classnames";
 import * as styles from "./gif.module.css";
 
 export default function Gif({ src, alt, className, width }) {
   return (
     <div className={styles.container}>
-      <img width={width} src={src} alt={alt} className={cn(className)} />
+      <img width={width} src={src} alt={alt} className={className} />
     </div>
   );
 }
